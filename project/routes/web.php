ebe0cdd003452b512aa99c360e09c6a3eb7d46ba
Dnell65 <dnell2182@gmail.com>
2022-12-05 12:14:21 -0500

    html/css

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/roster', function () {
+    return view('roster');
+});
+Route::get('/role', function () {
+    return view('role');
+});
