9047aa74bdcf33a1cef58026bcde75262b75e84e
Connor-Csoky <connorcsoky@gmail.com>
2022-12-07 18:24:32 -0500

    finished the role page

--- project/resources/views/role.blade.php
+++ project/resources/views/role.blade.php
@@ -1,105 +1,124 @@
 <html>
 <body>
+    <form action="/api/role" method="POST">
+    <div class="text">
+        <div>
+            <label for="emp_id">Role</label>
+            <input id="role" type="text" name="role">
+        </div>
+        <div>
+            <label for="New_Salary">Accesss Level</label>
+            <select id="access" type="text" name="accessLevel">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+        </div>
+    </div>
     <table>
         <tr>
             <th>Role</th>
             <th>Access Level</th>
         </tr>
+        <?php for($i=0;$i<count($_SESSION['role']);$i++){ ?>
         <tr>
-            <td></td>
+            <td><?php echo $_SESSION['role'][$i]->role ?></td>
+            <td><?php echo $_SESSION['role'][$i]->accessLevel ?></td>
         </tr>
+        <?php } ?>
     </table>
-
-<div class="grid1">
-    <label for="New_role">New Role</label>
-    <form>
-        <input type="text">
+    <div class="grid2">
+        <div class="click">
+            <input value="Ok" class="submit" type="submit">  
+            <a href="">
+                <button>Cancel</button>
+            </a>
+        </div>
+    </div>
     </form>
-</div>
-<div class="grid">
-    <label for="Access_level">Access Level</label>
-    <form>
-        <input type="text">
-    </form>
-</div>
-<div class="grid2">
-<div class="ok">
-    <a href="">
-        <button>Ok</button>
-    </a>
-</div>
-<div class="cancel">
-    <a href="">
-        <button>Cancel</button>
-    </a>
-</div>
-</div>
 
 </body>
 </html> 
 <style>
-    .grid{
-        display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        gap: 10px;
-        margin-top: 15px;
-    }
-    label{
-        background-color: blue;
-        text-align: center;
-        border:2px solid black;
-        border-top-right-radius: 10px;
-        color: white;
-        padding: 2%;
-        padding-top: 20px;
-        font-size: 25px;
-    }
-    .grid1{
-        display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        gap: 10px;
-        margin-top: 25%;
-    }
-    input{
-        margin-top: 10px;
-        margin-left: 10px;
-        border: 7px solid blue;
-        width: 250px;
-        height: 50px;
-    }
-    .grid2{
-        display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        gap: 10px;
-    }
-    button{
-        background-color: #2E8B57;
-        border-color: black;
-        color: white;
-        padding: 20px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        width: 150px;
-        margin-top: -80px;
-    }
-    .ok{
-        margin-left: 900px;
-    }
-    .cancel{
-        margin-left: 30px;
-    }
-    table{
-        border: 1px solid ;
-        background-color: lightgray;
-        width: 500px;
-        height: 50px;
-        margin-left: auto;
-        margin-right: auto; 
+    table {
+            overflow-y: scroll;
+            max-height: 300px;
+            min-height: fit-content;
+            display:block;
         }
-   th{
-    height: 50px;
-    width: 25px;
-   }
+        table, tr, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+            width: 1000px;
+            margin-left: auto;
+            margin-right: auto; 
+        }
+        tr, th {
+            height: 25px;
+        }
+        th {
+            background-color: lightgray;
+        }
+        input{
+            margin-left: 10px;
+            border: 7px solid blue;
+            width: 250px;
+            height: 50px;
+        }
+        button{
+            margin-top: 1rem;
+            background-color: #2E8B57;
+            height: 50px;
+            width: 100px;
+            cursor:pointer;
+            color:white;
+            border-style: none;
+        }
+        .click{
+            display: flex;
+            gap: 2%;
+            justify-content: center;
+        }
+        label{
+            background-color: blue;
+            border-top-right-radius: 10px;
+            border-color: black;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            width: 50px;
+            
+        }
+        .text{
+            display: flex;
+            gap: 2rem;
+            justify-content: center;
+            align-items: center;
+            margin-top: 2rem;;
+            margin-bottom: 1rem;
+        }
+        .text > div {
+            display: flex;
+            align-items:center;
+        } 
+        select {
+            margin-left: 10px;
+            border: 7px solid blue;
+            width: 250px;
+            height: 50px;
+        }
+        .submit {
+            margin-top: 1rem;
+            background-color: #2E8B57;
+            height: 50px;
+            width: 100px;
+            cursor:pointer;
+            color:white;
+            border-style: none;
+        }
 </style>
